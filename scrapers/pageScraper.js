347185779ed99762583b57e018d182176481ecd9
alexis@thinkcerca.com <alexis@thinkcerca.com>
2023-10-18 18:29:22 -0300

    scrapper works and removed node_modules

--- scrapers/pageScraper.js
+++ scrapers/pageScraper.js
@@ -6,25 +6,30 @@ const scraperObject = {
     //Navigate to site
 		console.log(`Navigating to ${this.url}...`);
 		await page.goto(this.url, {timeout: 0});
+    console.log('Cargó la página, busco selector')
     //Wait for page to load
     await page.waitForSelector('[class*="contenedorprincipal"]');
-    // //Click on "Ver más" button
-    // const clickSeeMore = async () => {
-    //   const seeMoreButton = await page.$('span.vinculos.vermas_home');
-    //   if (seeMoreButton) {
-    //     await seeMoreButton.click();
-    //     return true;
-    //   }
-    //   return false;
-    // };
-    // //Load all plays
-    // let morePlays = true;
-    // while (morePlays) {
-    //   morePlays = await clickSeeMore();
-    //   if (morePlays) {
-    //     await page.waitForTimeout(2000);
-    //   }
-    // }
+    console.log('Encontré selector')
+    //Click on "Ver más" button
+    const clickSeeMore = async () => {
+      const seeMoreButton = await page.$('span.vinculos.vermas_home');
+      if (seeMoreButton) {
+        await seeMoreButton.click();
+        return true;
+      }
+      return false;
+    };
+    //Load all plays
+    let morePlays = true;
+    while (morePlays) {
+      console.log('comienza click more')
+      morePlays = await clickSeeMore();
+      console.log('termina click more')
+      if (morePlays) {
+        await page.waitForTimeout(2000);
+      }
+    }
+    console.log('no hay más obras!!!')
 		let parentElements = await page.$$eval('img.w-100', elements => {
       elements.forEach(async (element) => {
         console.log('Element is:')
